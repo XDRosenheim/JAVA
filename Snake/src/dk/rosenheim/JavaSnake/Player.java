a47de24e27545e7dffefb4a7710e73d72f6e663b
Rosenheim <XDRosenheim@users.noreply.github.com>
2015-07-28 12:21:00 +0200

    More :snake:

--- Snake/src/dk/rosenheim/JavaSnake/Player.java
+++ Snake/src/dk/rosenheim/JavaSnake/Player.java
@@ -2,30 +2,26 @@ package dk.rosenheim.JavaSnake;
 
 import java.util.Scanner;
 
-/**
- * Created by HVF-E308 on 28-07-2015.
- */
 public class Player {
 
     public static String playerName;
-    public static int lives, score;
 
     public static void main(String[] args){
-
         if (playerName == null) {
-            int nameLenght = 0;
-            while (nameLenght != 4){
+            int nameLenght = 0, nameLenghtMust = 4;
+            while (nameLenght != nameLenghtMust){
                 System.out.print("Enter your name: ");
                 Scanner playerInput = new Scanner(System.in);
                 playerName = playerInput.next().toUpperCase();
-                if (playerName.length() == 4) {
+                if (playerName.length() == nameLenghtMust) {
                     System.out.println("Welcome, " + playerName + ". Let's get started!");
                     nameLenght = playerName.length();
                 }
                 else {
-                    System.out.println("Your name must be 3 chars long.");
+                    System.out.println("Your name must be " + nameLenghtMust + " chars long.");
                 }
             }
         }
+        System.out.println("-- Go to game --");
     }
 }
