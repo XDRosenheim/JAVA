5bf9add12d2b4799e6b748278f6e8a2100a500d4
Rosenheim <XDRosenheim@users.noreply.github.com>
2015-07-29 12:41:43 +0200

    New thing.

--- Snake/src/dk/rosenheim/JavaSnake/mainMenu.java
+++ Snake/src/dk/rosenheim/JavaSnake/mainMenu.java
@@ -6,15 +6,23 @@ public class mainMenu {
     public static void main(String[] args){
         while (true){
             options.clearConsole();
-            int selection;
+            int selection = -1;
             IHateLongCommands.echo("##   Snake   ##");
             IHateLongCommands.echo("[1] Start game.");
             IHateLongCommands.echo("[2] Options.");
             IHateLongCommands.echo("[0] Exit.");
             IHateLongCommands.echo("");
             IHateLongCommands.echo("Selection: ");
-            Scanner scanner = new Scanner(System.in);
-            selection = scanner.nextInt();
+
+	        while(selection == -1){
+		        try {
+			        Scanner scanner = new Scanner(System.in);
+			        selection = scanner.nextInt();
+		        }
+		        catch(Exception e){
+					System.out.println("Not a valid selection. Try again.");
+		        }
+		    }
 
             switch (selection){
                 case 1:
---
+++ character/src/com/company/Character.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Character {
+	public String name;
+	public Boolean gender;
+	public int level;
+
+	public Character(String name, Boolean gender, int level){
+		this.name = name;
+		this.gender = gender;
+		this.level = level;
+	}
+}
