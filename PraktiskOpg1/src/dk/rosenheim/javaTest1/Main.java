dc8bec12da4a3e6d82853085abd648498b8c578d
Rosenheim <XDRosenheim@users.noreply.github.com>
2015-07-30 11:46:02 +0200

    That was actually quite easy.
    Make a program where the user is asked about how many numbers he/she
    wants to input and have processed.
    Create an array with the size of what the user answered.
    Ask the user to input numbers, one number at the time, and put it into
    the array.
    
    Find the following in the array:
    • The highest number
    • The lowest number
    • The sum of the numbers
    • The average of the numbers
    • The total amout of numbers in the array.
    Print these...

---
+++ PraktiskOpg1/src/dk/rosenheim/javaTest1/Main.java
@@ -0,0 +1,51 @@
+package dk.rosenheim.javaTest1;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		int[] numbers;
+		int higheast, loweast, sum = 0, avg; // Hehe, east.
+		while (true) {
+			try {
+				System.out.println("How many number do you want in the array?");
+				int arraySize = input.nextInt();
+
+				numbers = new int[arraySize];
+
+				break;
+			} catch(Exception e) {
+				System.out.println("Only numbers, mate.");
+				input.next();
+			}
+		}
+	    for(int i = 0; i < numbers.length; i++) {
+			while(true){
+				try{
+					System.out.print("Enter the number you want in slot " + i + ": ");
+					numbers[i] = input.nextInt();
+					break;
+				}catch(Exception e){
+					System.out.println("Only numbers, mate.");
+					input.next();
+				}
+			}
+	    }
+	    higheast = numbers[0];
+	    loweast = numbers[0];
+	    for(int i = 0; i < numbers.length; i++) {
+		    if(loweast > numbers[i]) loweast = numbers[i];
+		    else if(higheast < numbers[i]) higheast = numbers[i];
+
+		    sum = sum + numbers[i];
+	    }
+	    avg = sum / numbers.length;
+	    System.out.println("Highest number in array: " + higheast);
+	    System.out.println("Lowest number in array: " + loweast);
+	    System.out.println("All numbers put together: " + sum);
+	    System.out.println("The avgerage of all numbers: " + avg);
+		System.out.print("Amount of numbers in array: " + numbers.length);
+    }
+}
