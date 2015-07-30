c5c1ef49b50fa8d850532517202cf6fe151f5941
Rosenheim <XDRosenheim@users.noreply.github.com>
2015-07-30 08:41:07 +0200

    All done!
    Motorcicle.

--- Motorcycle/src/dk/rosenheim/motorcycle/Main.java
+++ Motorcycle/src/dk/rosenheim/motorcycle/Main.java
@@ -5,6 +5,25 @@ public class Main {
     public static void main(String[] args) {
 		Motorcycle myBike = new Motorcycle();
 
-        myBike.setName("Honda")
+	    myBike.setName("Honda");
+	    myBike.start();
+	    myBike.toString();
+	    myBike.shiftGearsUp();
+		myBike.setRpm(2000);
+	    myBike.shiftGearsUp();
+	    myBike.setRpm(3500);
+	    myBike.shiftGearsUp();
+	    myBike.setRpm(4000);
+	    myBike.shiftGearsUp();
+	    myBike.shiftGearsUp();
+	    myBike.setRpm(3500);
+	    myBike.toString();
+	    myBike.shiftGearDown(2);
+	    myBike.setRpm(1200);
+	    myBike.toString();
+	    myBike.shiftGearDown(0);
+	    myBike.setRpm(1000);
+	    myBike.stop();
+	    myBike.toString();
     }
 }
--- Motorcycle/src/dk/rosenheim/motorcycle/Motorcycle.java
+++ Motorcycle/src/dk/rosenheim/motorcycle/Motorcycle.java
@@ -13,30 +13,32 @@ public class Motorcycle {
 		this.name = name;
 	}
 
+	// Start and stop
 	public void start() {
 		this.started = true;
 		this.gear = 0;
 		this.rpm = 1000;
 	}
-
 	public void stop() {
 		this.started = false;
 		this.gear = 0;
 		this.rpm = 0;
 	}
 
+	// Get engine status as true/false
 	public Boolean isStarted() {
 		return this.started;
 	}
 
+	// Set/get Name
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public String getName() {
 		return this.name;
 	}
 
+	// Set/get RPM
 	public void setRpm(int rpm) {
 		if (rpm > 8000) {
 			this.rpm = 8000;
@@ -46,46 +48,52 @@ public class Motorcycle {
 			this.rpm = rpm;
 		}
 	}
-
 	public int getRpm() {
 		return this.rpm;
 	}
 
+	// Get speed
 	public int getSpeed() {
 		int speed;
 		speed = this.rpm * this.gear / 200;
 		return speed;
 	}
 
+	// Change gear
 	public void shiftGearsUp() {
-		if (this.isStarted() == true) {
+		if (this.isStarted()) {
 			if (this.gear < 5) {
-				this.gear = this.gear++;
+				this.gear = this.gear + 1;
 			}
 		}
 	}
-
 	public void shiftGearDown(int g) {
 		if (this.gear > g && g > 0) {
 			this.gear = g;
 		}
 	}
 
+	// Get gear
 	public int getGear(){
 		return this.gear;
 	}
 
+	// Output all stats
+	@Override
 	public String toString(){
-		if(this.isStarted() == false) {
+		System.out.println("Status of \"" + getName() + "\"");
+		if(!this.isStarted()) {
 			System.out.println("Engine is off.");
 		}
 		else {
 			System.out.println("Engine status:  On");
-			System.out.println("      RPM:      " + this.getRpm());
-			System.out.println(" Current gear:  " + this.getGear());
-			System.out.println("     Speed:     " + this.getSpeed() + "km/h");
-
+			System.out.println("      RPM:      " + getRpm());
+			System.out.println(" Current gear:  " + getGear());
+			System.out.println("     Speed:     " + getSpeed() + "km/h");
+			if(getSpeed() == 0){
+				System.out.println("Ya standing still, mate.");
+			}
 		}
-
+		return null;
 	}
 }
